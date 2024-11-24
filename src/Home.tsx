69f4d3ab56a3036ac6743eeccf4ae09a8b0ae088
kozzztia <kozzztia@gmail.com>
2024-11-24 15:52:17 +0200

    fix navs

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,8 +2,8 @@
 const Home = () => {
   return (
     <>
-        <div id="home" style={{height: '60vh', paddingTop: 'var(--header-height)'}}>
-            <h1>Home</h1>
+        <div id="main" style={{height: '60vh', paddingTop: 'var(--header-height)'}}>
+            <h1>main</h1>
         </div>
         <div id="about" style={{height: '60vh', paddingTop: 'var(--header-height)'}}>
             <h2>about</h2>
--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,43 +1,48 @@
 
 
 export const navLinks = [
-    {
-      id : 1,
-      url: 'home',
-      title: 'Home',
-    },
-    {
-      id : 2,
-      url: 'about',
-      title: 'About',
-    },
-    {
-      id: 3,
-      url: 'projects',
-      title: 'Projects',
-    },
-    {
-      id : 4,
-      url: 'contact',
-      title: 'Contact',
-    },
+  {
+    id: 1,
+    url: 'main',
+    title: 'Main',
+  },
+  {
+    id: 2,
+    url: 'about',
+    title: 'About',
+  },
+  {
+    id: 3,
+    url: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 4,
+    url: 'contact',
+    title: 'Contact',
+  },
 ]
 
 export const categoryLinks = [
-    {
-      id : 1,
-      url: 'mtb',
-      title: 'Mountain Bikes',
-    },
-    {
-      id : 2,
-      url: 'road',
-      title: 'Road Bikes',
-    },
-    {
-      id : 3,
-      url: 'bmx',
-      title: 'BMX Bikes',
-    },
+  {
+    id: 0,
+    url: '',
+    title: 'Main',
+  },
+  {
+    id: 1,
+    url: 'mtb',
+    title: 'Mountain Bikes',
+  },
+  {
+    id: 2,
+    url: 'road',
+    title: 'Road Bikes',
+  },
+  {
+    id: 3,
+    url: 'bmx',
+    title: 'BMX Bikes',
+  },
 ]
 
