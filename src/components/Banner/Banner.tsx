b4b4e5c12a6fbb3103e9b17c1d3af94cae74f46b
kozzztia <kozzztia@gmail.com>
2024-11-25 18:29:04 +0200

    dictionary

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,10 +1,11 @@
 import style from './style.module.css'
 import {navLinks} from '../../consts/consts'
+import {getDictionary} from '../../consts/dictionary'
 
 const Banner = () => {
   return (
     <div id={navLinks[0].url} className={style.banner}>
-        
+        <h2>{getDictionary('bannerTitle')}</h2>
         
     </div>
   )
