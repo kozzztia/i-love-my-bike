c93e6e8cba217221e66d538bdeaced72d12ce72a
kozzztia <kozzztia@gmail.com>
2024-11-25 22:51:33 +0200

    promotion fetch

--- src/consts/dictionary.ts
+++ src/consts/dictionary.ts
@@ -1,8 +1,10 @@
 
+
 const dictinary = {
     header: "header",
     navigation: "navigation",
-    bannerTitle: "banner",
+    bannerTitle: "Promotion",
+    topThree: "Best three bikes in the world"
 };
 
 export const getDictionary = (key: keyof typeof dictinary): string => dictinary[key];
