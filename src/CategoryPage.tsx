cfa68dca5ea48d7b4f00107aedf5e4e05ecb09e3
kozzztia <kozzztia@gmail.com>
2024-12-02 22:16:05 +0200

    preloader

--- src/CategoryPage.tsx
+++ src/CategoryPage.tsx
@@ -2,12 +2,19 @@ import {useParams } from 'react-router-dom'
 import PageContainer from './components/ui-kit/PageContainer/PageContainer';
 import { useCategoryBikes } from './state/StateProvider/useStateValue';
 import CategoryList from './components/CategoryList/CategoryList';
+import { useEffect } from 'react';
+import Preloader from './components/ui-kit/Preloader/Preloader';
 
 
 const CategoryPage = () => {
   const { category } = useParams();
   const categoryBikes = useCategoryBikes(category!);
-
+  useEffect(()=>{
+    window.scrollTo(0, 0)
+  })
+  if(!categoryBikes){
+    return <Preloader color={'var(--element-background-color)'}/>
+  }
   return (
     <PageContainer hash="category" isSingle={true} isDecore={false}>
       <h2>Page: {category}</h2>
