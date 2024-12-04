8b824cb5caabdf514f4c577bafbcbcc4bff30633
kozzztia <kozzztia@gmail.com>
2024-12-04 20:49:00 +0200

    try to contained

--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -4,20 +4,21 @@ import { BannerBikeType } from '../../types/BikeType';
 import BikeLink from '../ui-kit/BikeLink/BikeLink';
 import style from './style.module.css'
 import { useLocation } from 'react-router-dom';
+import PageContainer from '../ui-kit/PageContainer/PageContainer';
 
 const CategoryList: React.FC<Props> = ({ categoryBikes, className }) => {
     const hash = useLocation().hash.slice(1);
     const [bikes, setBikes] = useState<BannerBikeType[] | []>([]);
     useEffect(() => {
-        if(!hash){       
-             setBikes(categoryBikes)
-        }else if(hash === 'best'){
+        if (!hash) {
+            setBikes(categoryBikes)
+        } else if (hash === 'best') {
             setBikes(categoryBikes.sort((a, b) => b.rating - a.rating))
-        }else if(hash === 'cheap'){
+        } else if (hash === 'cheap') {
             setBikes(categoryBikes.sort((a, b) => a.price - b.price))
-        }else if(hash === 'expensive'){
+        } else if (hash === 'expensive') {
             setBikes(categoryBikes.sort((a, b) => b.price - a.price))
-        }else{
+        } else {
             setBikes(categoryBikes)
         }
 
@@ -28,16 +29,18 @@ const CategoryList: React.FC<Props> = ({ categoryBikes, className }) => {
             {
                 bikes.map((bike) => (
                     <li key={bike.id}>
-                        <img src={bike.icon} alt={bike.name} width={100} height={100} />
-                        <p>{bike.title}</p>
-                        <p>{bike.price}</p>
-                        <p>{bike.rating}</p>
-                        <p>{bike.status}</p>
-                        <p>{bike.description}</p>
-                        <BikeLink title={`${getDictionary('linkTitle')} ${bike.name}`} url={`/${bike.category}/${bike.id}`} />
+                        <PageContainer isSingle={true} isDecore={false}>
+                            <img src={bike.icon} alt={bike.name} width={100} height={100} />
+                            <p>{bike.title}</p>
+                            <p>{bike.price}</p>
+                            <p>{bike.rating}</p>
+                            <p>{bike.status}</p>
+                            <p>{bike.description}</p>
+                            <BikeLink title={`${getDictionary('linkTitle')} ${bike.name}`} url={`/${bike.category}/${bike.id}`} />
+                        </PageContainer>
                     </li>
                 ))
-        }
+            }
         </ul>
     )
 }
