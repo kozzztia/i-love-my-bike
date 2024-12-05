90c651060a578b5bc73553d6e7e8f26b23d67ab8
kozzztia <kozzztia@gmail.com>
2024-12-05 17:34:58 +0200

    cancel

--- src/state/StateProvider/StateProvider.tsx
+++ src/state/StateProvider/StateProvider.tsx
@@ -8,33 +8,33 @@ export const StateProvider: React.FC<Props> = ({ children }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchBikes = async (retries = 5, delay = 2000) => {
-      for (let attempt = 1; attempt <= retries; attempt++) {
-        try {
-          const response = await fetch("https://funny-fudge-ddda7b.netlify.app/api/items");
-          if (!response.ok) {
-            throw new Error(`Error: ${response.status}`);
-          }
-          const data: BikeType[] = await response.json();
-          setBikes(data);
-          setError(null); // Очистить ошибку, если данные успешно загружены
-          return; // Успешная загрузка, выход из функции
-        } catch (error) {
-          if (attempt === retries) {
-            setError(error instanceof Error ? `Error: ${error.message}` : "Unknown error");
-          } else {
-            console.warn(`Retrying... (${attempt}/${retries})`);
-          }
-          // Ждем перед повторной попыткой
-          await new Promise((resolve) => setTimeout(resolve, delay));
+useEffect(() => {
+  const fetchBikes = async (retries = 5, delay = 2000) => {
+    for (let attempt = 1; attempt <= retries; attempt++) {
+      try {
+        const response = await fetch("https://funny-fudge-ddda7b.netlify.app/api/items");
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
         }
+        const data: BikeType[] = await response.json();
+        setBikes(data);
+        setError(null); // Очистить ошибку, если данные успешно загружены
+        return; // Успешная загрузка, выход из функции
+      } catch (error) {
+        if (attempt === retries) {
+          setError(error instanceof Error ? `Error: ${error.message}` : "Unknown error");
+        } else {
+          console.warn(`Retrying... (${attempt}/${retries})`);
+        }
+        // Ждем перед повторной попыткой
+        await new Promise((resolve) => setTimeout(resolve, delay));
       }
-      setIsLoading(false); // Остановить загрузку после всех попыток
-    };
-  
-    fetchBikes();
-  }, []);
+    }
+    setIsLoading(false); // Остановить загрузку после всех попыток
+  };
+
+  fetchBikes();
+}, []);
 
   return (
     <StateContext.Provider value={{ bikes, isLoading , error}}>
