3ca67604667b5cc7979093beb7e7beb746d9611b
kozzztia <kozzztia@gmail.com>
2024-12-11 18:49:52 +0200

    styled cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -3,25 +3,28 @@ import { BikeType } from '../../types/BikeType'
 import ToggleButton from '../ToggleButton/ToggleButton'
 import style from './style.module.css'
 
-const Cart: React.FC<Props> = ({data}) => {
-  return (
-    <ul className={style.list}>
-        {
-            data.map(bike => (
-                <li key={bike.id} className={style.cart}>
-                    <img src={bike.icon} alt={bike.name} />
-                    <h3>{bike.title}</h3>
-                    <p>{bike.price}$</p>
-                    <ToggleButton id={bike.id}/>
-                </li>
-            ))
-        }
-    </ul>
-  )
+const Cart: React.FC<Props> = ({ data }) => {
+    return (
+        <ul className={style.list}>
+            {
+                data.map(bike => (
+                    <li key={bike.id} className={style.cart}>
+                        <img src={bike.icon} alt={bike.name} />
+                        <article>
+                            <h3>{bike.title}</h3>
+                            <strong>{bike.price}$</strong>
+                            <p>{bike.description}</p>
+                            <ToggleButton id={bike.id} />
+                        </article>
+                    </li>
+                ))
+            }
+        </ul>
+    )
 }
 
 type Props = {
-    data: BikeType[] 
+    data: BikeType[]
 }
 
 export default Cart
