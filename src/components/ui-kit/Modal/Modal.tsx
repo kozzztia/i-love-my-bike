e0fd65891efbd7d566996d5a77d32046cb4cf1b1
kozzztia <kozzztia@gmail.com>
2024-12-13 16:42:20 +0200

    styled modal

--- src/components/ui-kit/Modal/Modal.tsx
+++ src/components/ui-kit/Modal/Modal.tsx
@@ -1,47 +1,66 @@
 import React, { useEffect } from 'react'
 import style from "./style.module.css"
 import ReactDOM from 'react-dom';
+import { getLocalBikes } from '../../../LocalStorage';
+import { useLocalBikes } from '../../../state/StateProvider/useStateValue';
+import BikeImage from '../BikeImage/BikeImage';
+import Preloader from '../Preloader/Preloader';
 
-function disableScroll() {
-    document.body.style.overflow = "hidden";
-  }
-  
-  function enableScroll() {
-    document.body.style.overflow = "";
-  }
-  
 
 const Modal: React.FC<Props> = ({ isOpen, setIsOpen }) => {
-    useEffect(() => {
-        if (isOpen) {
-          disableScroll();
-        } else {
-          enableScroll();
-        }
-      }, [isOpen]);
+  const ids = getLocalBikes();
+  const bikes = useLocalBikes(ids);
+  console.log(bikes)
 
-    if (!isOpen) return null;
-  
-    return ReactDOM.createPortal(
-      <div className={style.modal} onClick={()=>setIsOpen(false)}>
-        <div className={style.content} onClick={(e) => e.stopPropagation()}>
-          <button onClick={()=>setIsOpen(false)}>
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+  if (!bikes) return <Preloader />
+
+  return ReactDOM.createPortal(
+    <div className={style.modal} onClick={() => setIsOpen(false)}>
+      <div className={style.content} onClick={(e) => e.stopPropagation()}>
+        {
+          bikes.map((bike) => (
+            <div key={bike.id} className={style.item}>
+              <div className={style.info}>
+                <h4>{bike.title}</h4>
+                <p>{bike.price} $</p>
+              </div>
+              <BikeImage src={bike.icon} name={bike.name} className={style.image} />
+            </div>
+          ))
+        }
+        <div className={style.buttons}> 
+          <button
+            onClick={() => setIsOpen(false)}>
             back
           </button>
+          <p>
+            {bikes.reduce((bikes, bike) => bikes + bike.price, 0)} $
+          </p>
           <button>
             ok
           </button>
         </div>
-      </div>,
-      document.getElementById("modal-root") as HTMLElement
-    );
-  };
-  
+      </div>
+    </div>,
+    document.getElementById("modal-root") as HTMLElement
+  );
+};
+
 
 export default Modal
 
 
 type Props = {
-    isOpen: boolean;
-    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
