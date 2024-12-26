7d3fdedc8f2e8cfdd829b26cb7db1ef0ef7c445e
kozzztia <kozzztia@gmail.com>
2024-12-26 10:02:20 +0200

    fix contacts links

--- src/components/ui-kit/BikeSlider/BikeSlider.tsx
+++ src/components/ui-kit/BikeSlider/BikeSlider.tsx
@@ -1,19 +1,48 @@
-import React from 'react'
-import style from "./style.module.css";
+import React, { useState } from 'react';
+import style from './style.module.css';
 
-const BikeSlider: React.FC<Props> = ({ images, className="" }) => {
+const BikeSlider: React.FC<Props> = ({ images, className = '' }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    const prevSlide = () => {
+        setCurrentIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
+
     return (
-        <div className={[style.slider, className].join(" ")}>
-            {
-                images.map((image, index) => <img key={index} src={image}/>)
-            }
-        </div>
-    )
-}
+        <div className={[style.sliderWrapper, className].join(' ')}>
+            <div className={style.slider} style={ {width: `${images.length * 100}%`}}>
+                {images.map((image, index) => (
+                    <div
+                        key={index}
+                        className={`${style.slide} ${
+                            index === currentIndex ? style.active : ''
+                        }`}
+                        style={{ backgroundImage: `url(${image})`}}
+                    />
+                ))}
+            </div>
 
-export default BikeSlider
+
+
+            <button className={style.prevButton} onClick={prevSlide}>
+                ‹
+            </button>
+            <button className={style.nextButton} onClick={nextSlide}>
+                ›
+            </button>
+        </div>
+    );
+};
+
+export default BikeSlider;
 
 type Props = {
     images: string[];
     className?: string;
-}
+};
